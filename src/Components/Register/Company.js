58fbef1006c6ba9fd05bfdf1567060dd5d0d7b84
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2022-12-07 21:44:28 +0530

    message show to modal

--- src/Components/Register/Company.js
+++ src/Components/Register/Company.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
+import OtherCompany from './OtherCompany';
 
 const Company = (props) => {
-
+const {setCompanyCode,setCompanyName,setCompanyLogo}=props
     const [company , setcompany] = useState([]);
   const [companyId,setcompanyId]=useState('')
   const getCompany = () =>{
@@ -21,15 +22,25 @@ const Company = (props) => {
 
   return (
     <>
-      <div className='flex flex-col gap-1'>
-        <label htmlFor="company">Company</label>
+      <div className='flex flex-col gap-5'>
+        <div className="flex flex-col gap-1">
+        <label htmlFor="company" className='font-medium'>Company</label>
         <select value={companyId} name="company" id="company" className='w-full py-1.5 outline-none border-2' 
           onChange={getCompanyValue}>
             <option value="0">Select Company</option>
             {company.map(e=>(
                 <option key={e._id} value={e._id}>{e.name}</option>
             ))}
+            <option value="newCompany">New Company</option>
         </select>
+        </div>
+        {
+          companyId ==="newCompany" ? <OtherCompany
+          setCompanyName={setCompanyName}
+          setCompanyCode={setCompanyCode}
+          setCompanyLogo={setCompanyLogo}
+          /> : <></>
+        }
       </div>
     </>
   )
