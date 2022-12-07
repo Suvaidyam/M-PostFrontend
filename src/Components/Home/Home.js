2997f66ac6fc93cdc0958efa472d07c0c9bf057e
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2022-12-07 12:16:55 +0530

    add enviroment page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeLeftBar from "../HomeLeftBar/HomeLeftBar";
 import HomeRightBar from "../HomeRightBar/HomeRightBar";
 import DataProvider from "../Context/DataProvider";
@@ -6,8 +6,10 @@ import { AiFillCaretDown ,AiOutlineEye} from "react-icons/ai";
 import { BsCode,BsCaretRight} from "react-icons/bs";
 import { FiTrash2} from "react-icons/fi";
 import { BiHelpCircle} from "react-icons/bi";
+import SetEnviroment from "../SetEnviroment/SetEnviroment";
 
 const Home = () => {
+  const [enviroment, setEnviroment] = useState(false)
   return (
     <>
       <DataProvider>
@@ -32,16 +34,19 @@ const Home = () => {
               {/* Right */}
               <HomeRightBar />
             </div>
-            <div className="w-[4%] h-full flex flex-col justify-between items-center py-3 ">
-              <div className=" flex flex-col gap-5">
-                <AiOutlineEye className="cursor-pointer"/>
+            <div className="w-[4%] h-full flex flex-col justify-between items-center py-3 relative">
+              <div className=" flex flex-col items-center justify-center gap-5">
+                <div className={`${enviroment===true?'bg-slate-200 rounded-sm':null}`}>
+                 <p className="p-2 hover:bg-slate-200"> <AiOutlineEye className={`cursor-pointer`} onClick={()=>setEnviroment(!enviroment)}/></p>
+                </div>
                 <BsCode className="cursor-pointer"/>
               </div>
+              {/* Enviroment */}
+              {enviroment===true?<SetEnviroment/>:null}
               <div className="flex flex-col gap-5 ">
                 <BsCaretRight className="cursor-pointer"/>
                 <FiTrash2 className="cursor-pointer"/>
                 <BiHelpCircle className="cursor-pointer"/>
-
               </div>
             </div>
           </div>
