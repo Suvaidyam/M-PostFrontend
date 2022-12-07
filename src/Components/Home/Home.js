e7bc264feea338b061b4c8945058565a2db0c410
ani07ket <aniket.suvaidyam@gmail.com>
2022-12-07 12:57:07 +0530

    login page sucess to connect

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,14 +2,16 @@ import React from "react";
 import HomeLeftBar from "../HomeLeftBar/HomeLeftBar";
 import HomeRightBar from "../HomeRightBar/HomeRightBar";
 import DataProvider from "../Context/DataProvider";
-import { AiFillCaretDown ,AiOutlineEye} from "react-icons/ai";
-import { BsCode,BsCaretRight} from "react-icons/bs";
-import { FiTrash2} from "react-icons/fi";
-import { BiHelpCircle} from "react-icons/bi";
+import { AiFillCaretDown, AiOutlineEye } from "react-icons/ai";
+import { BsCode, BsCaretRight } from "react-icons/bs";
+import { FiTrash2 } from "react-icons/fi";
+import { BiHelpCircle } from "react-icons/bi";
+import Header from "../Header/Header";
 
 const Home = () => {
   return (
     <>
+      <Header />
       <DataProvider>
         <div className="w-full h-[88.5vh] ">
           <div className="w-full h-full overflow-hidden max-w-[1720px] mx-auto flex">
@@ -22,26 +24,30 @@ const Home = () => {
             <div className="w-[66%] bg-gray-100">
               {/* header */}
               <div className="w-full h-10 bg-white shadow-inner flex">
-                  <div className="w-[80%] border"></div>
-                  <div className="w-[20%] border flex justify-center items-center gap-2">
-                    <input type="text" name="" id="" defaultValue={'No Enviroment'}
-                     className='w-[70%] outline-none text-sm'/>
-                    <AiFillCaretDown className='text-[9px] cursor-pointer'/>
-                  </div>
+                <div className="w-[80%] border"></div>
+                <div className="w-[20%] border flex justify-center items-center gap-2">
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    defaultValue={"No Enviroment"}
+                    className="w-[70%] outline-none text-sm"
+                  />
+                  <AiFillCaretDown className="text-[9px] cursor-pointer" />
+                </div>
               </div>
               {/* Right */}
               <HomeRightBar />
             </div>
             <div className="w-[4%] h-full flex flex-col justify-between items-center py-3 ">
               <div className=" flex flex-col gap-5">
-                <AiOutlineEye className="cursor-pointer"/>
-                <BsCode className="cursor-pointer"/>
+                <AiOutlineEye className="cursor-pointer" />
+                <BsCode className="cursor-pointer" />
               </div>
               <div className="flex flex-col gap-5 ">
-                <BsCaretRight className="cursor-pointer"/>
-                <FiTrash2 className="cursor-pointer"/>
-                <BiHelpCircle className="cursor-pointer"/>
-
+                <BsCaretRight className="cursor-pointer" />
+                <FiTrash2 className="cursor-pointer" />
+                <BiHelpCircle className="cursor-pointer" />
               </div>
             </div>
           </div>
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,68 +1,98 @@
-import axios from 'axios'
-import React, { useState } from 'react'
-// import { useNavigate } from 'react-router-dom'
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const [check, setCheck] = useState(true);
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [msg, setMsg] = useState('')
-  const [check, setCheck] = useState(true)
+  const navigate = useNavigate();
 
-  // const navigate = useNavigate();
+  const save = () => {
+    console.log({ email, password });
+    axios
+      .post(`http://localhost:4000/auth/login`, {
+        email: email,
+        password: password,
+      })
+      .then((res) => {
+        sessionStorage.setItem("token", res.data.token);
+        if (res.data.token) {
+          navigate("/workSpace");
+          let token = res.data.token;
+          let payload = token.split(".");
+          let data = atob(payload[1]);
+          // dispatch(ProfileDetailsAction(data))
 
-  const save =()=>{
-    console.log({email,password})
-    axios.post(`http://localhost:4000/auth/login`,
-      { email: email, password: password },
-    )
-    .then((res) => {
-      sessionStorage.setItem('token', (res.data.token))
-      if (res.data.token) {
-       
-        // navigate('/')
-        let token = res.data.token
-        let payload = token.split(".")
-        let data = atob(payload[1])
-        // dispatch(ProfileDetailsAction(data))
-
-        sessionStorage.setItem('paylode', data)
-      } else {
-        console.log("unauthorized")
-      }
-    })
-    .catch((err) => {
-      setMsg(err.response.data.message)
-    });
-  }
+          sessionStorage.setItem("paylode", data);
+        } else {
+          console.log("unauthorized");
+        }
+      })
+      .catch((err) => {
+        setMsg(err.response.data.message);
+      });
+  };
   return (
     <>
       <div className="w-full flex flex-col gap-5">
         <div className="flex flex-col gap-1">
-          <label htmlFor="email" className='font-medium'>Email or Usename</label>
-          <input type="text" id='email' className='border-2 outline-none w-full py-1 px-2'
-            placeholder='Enter Email or Username' onChange={(e)=>setEmail(e.target.value)} />
+          <label htmlFor="email" className="font-medium">
+            Email or Usename
+          </label>
+          <input
+            type="text"
+            id="email"
+            className="border-2 outline-none w-full py-1 px-2"
+            placeholder="Enter Email or Username"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className="flex flex-col gap-1">
-          <label htmlFor="password" className='font-medium'>Password</label>
-          <input type="text" id='password' className='border-2 outline-none w-full py-1 px-2'
-            placeholder='Enter Password' onChange={(e)=>setPassword(e.target.value)}/>
+          <label htmlFor="password" className="font-medium">
+            Password
+          </label>
+          <input
+            type="text"
+            id="password"
+            className="border-2 outline-none w-full py-1 px-2"
+            placeholder="Enter Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div className="flex justify-between">
-          <div className='flex items-center gap-1'>
-            <input type="checkbox" id='check' className='border-2 outline-none'
-              placeholder='Enter Password' onClick={()=>setCheck(!check)} />
-            <label htmlFor="check" className='text-sm cursor-pointer'>I agree with <span className='text-red-500'>terms & conditions</span></label>
+          <div className="flex items-center gap-1">
+            <input
+              type="checkbox"
+              id="check"
+              className="border-2 outline-none"
+              placeholder="Enter Password"
+              onClick={() => setCheck(!check)}
+            />
+            <label htmlFor="check" className="text-sm cursor-pointer">
+              I agree with{" "}
+              <span className="text-red-500">terms & conditions</span>
+            </label>
           </div>
-          <a href="#" className='text-sm text-blue-600'>Forget Password?</a>
+          <a href="#" className="text-sm text-blue-600">
+            Forget Password?
+          </a>
         </div>
-        <div className='w-full flex justify-end'>
-          <button disabled={check} className={`${check===false?'bg-blue-600':'bg-blue-200'}
-            py-2 text-white text-sm px-10 rounded-sm`} onClick={save}>LOGIN</button>
+        <div className="w-full flex justify-end">
+          <button
+            disabled={check}
+            className={`${check === false ? "bg-blue-600" : "bg-blue-200"}
+            py-2 text-white text-sm px-10 rounded-sm`}
+            onClick={save}
+          >
+            LOGIN
+          </button>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
