95d8017bcdeb825d37de435962a27d8d3ae9d02c
ani07ket <aniket.suvaidyam@gmail.com>
2023-01-10 13:21:05 +0530

    all component api loader intergreated

--- src/Components/Home/LeftBody/EnvironmentBody/EnvironmentBody.js
+++ src/Components/Home/LeftBody/EnvironmentBody/EnvironmentBody.js
@@ -1,16 +1,18 @@
 import React, { useContext, useEffect, useState } from "react";
 import http from "../../../../Services/http";
 import BodyHead from "../BodyHead/BodyHead";
-import {BiDotsHorizontalRounded} from 'react-icons/bi'
+import { BiDotsHorizontalRounded } from "react-icons/bi";
 import { useDispatch, useSelector } from "react-redux";
 import { Tabs } from "../../../../Redux/Action/Tabs";
 import { AddRequest } from "../../../../Redux/Action/AddRequest";
 import MoreAction from "../MoreAction/MoreAction";
 import { DataContext } from "../../../Context/DataProvider";
+import { CollectionLoader } from "../../../Loader/Loader";
 
 const EnvironmentBody = () => {
-  const [newEnviroment, setNewEnviroment] = useState([])
-  const [open, setOpen] = useState(false)
+  const [newEnviroment, setNewEnviroment] = useState([]);
+  const [loader, setLoader] = useState(true);
+  const [open, setOpen] = useState(false);
   const { setcolId } = useContext(DataContext);
 
   const dispatch = useDispatch();
@@ -43,6 +45,9 @@ const EnvironmentBody = () => {
       url: `${process.env.REACT_APP_BASEURL}/environment`,
     })
       .then((res) => {
+        setTimeout(() => {
+          setLoader(false);
+        }, 1000);
         setNewEnviroment(res.data.environment);
       })
       .catch((err) => {
@@ -55,23 +60,47 @@ const EnvironmentBody = () => {
       getData();
     };
   }, []);
- 
+
   return (
     <div className="w-full">
-      <BodyHead {...{postData}}/>
-        <div className="w-full relative">
-        {newEnviroment.map(e=>(
-            <div key={e._id} onClick={handleRequest(e)} className="w-full border-b hover:bg-gray-200
-            flex group relative">
-             <p className="w-full px-4 py-1.5 text-sm cursor-pointer "  onClick={() => dispatch(AddRequest(e._id))}>{e.name}</p>
-              <p className=" absolute right-2 flex justify-end top-2"  onClick={()=>setcolId(e)}>
-                <BiDotsHorizontalRounded className="cursor-pointer hidden group-hover:block" 
-                onClick={()=>setOpen(!open)} />
-              </p>
-           </div>
-        ))}
-           {open===true?<MoreAction className='absolute right-2'/>:null}
+      <BodyHead {...{ postData, title: "create environment" }} />
+      {loader === true ? (
+        <>
+          {newEnviroment.map((e) => (
+            <CollectionLoader key={e._id} />
+          ))}
+        </>
+      ) : (
+        <>
+          <div className="w-full relative">
+            {newEnviroment.map((e) => (
+              <div
+                key={e._id}
+                onClick={handleRequest(e)}
+                className="w-full border-b hover:bg-gray-200
+            flex group relative"
+              >
+                <p
+                  className="w-full px-4 py-1.5 text-sm cursor-pointer "
+                  onClick={() => dispatch(AddRequest(e._id))}
+                >
+                  {e.name}
+                </p>
+                <p
+                  className=" absolute right-2 flex justify-end top-2"
+                  onClick={() => setcolId(e)}
+                >
+                  <BiDotsHorizontalRounded
+                    className="cursor-pointer hidden group-hover:block"
+                    onClick={() => setOpen(!open)}
+                  />
+                </p>
+              </div>
+            ))}
+            {open === true ? <MoreAction className="absolute right-2" /> : null}
           </div>
+        </>
+      )}
     </div>
   );
 };
--- src/Components/Home/RightBar/SetEnviroment/SetEnviroment.js
+++ src/Components/Home/RightBar/SetEnviroment/SetEnviroment.js
@@ -1,34 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch ,useSelector} from 'react-redux';
-import { Tabs } from '../../../../Redux/Action/Tabs';
-import http from '../../../../Services/http';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Tabs } from "../../../../Redux/Action/Tabs";
+import http from "../../../../Services/http";
+import { EnvLoader } from "../../../Loader/Loader";
 
 const SetEnviroment = () => {
-
-  const [newEnviroment, setNewEnviroment] = useState([])
+  const [newEnviroment, setNewEnviroment] = useState([]);
+  const [loader, setLoader] = useState(true);
 
   let tabs = useSelector((state) => state.TabsReducer);
-  const dispatch =useDispatch()
+  const dispatch = useDispatch();
   const newInvObj = {
     name: "New Environment",
-    variable: '',
+    variable: "",
     type: "request",
     method: "NA",
     parent: null,
   };
-  const handleNewInv=()=>{
+  const handleNewInv = () => {
     let el = { ...newInvObj, _id: tabs.length };
-    el.name = el.name ;
+    el.name = el.name;
     tabs.push(el);
     dispatch(Tabs(tabs));
     // dispatch(AddRequest(el._id));
-  }
+  };
   const getData = () => {
     http({
       method: "get",
       url: `${process.env.REACT_APP_BASEURL}/environment`,
     })
       .then((res) => {
+        setTimeout(() => {
+          setLoader(false);
+        }, 1000);
         setNewEnviroment(res.data.environment);
       })
       .catch((err) => {
@@ -43,72 +47,132 @@ const SetEnviroment = () => {
   }, []);
   return (
     <>
-        <div className="w-[700px] h-[400px] border bg-gray-50 shadow-xl rounded-md absolute
-         right-[58px] top-11 py-2 flex flex-col justify-between">
-          <div className="w-full  flex flex-col">
-            <div className="bg-gray-100 w-full  flex flex-col gap-5  p-2">
-            <div className="flex justify-between">
-              <p className='text-sm'>Enviroment</p>
-              <p className='text-sm text-blue-500 cursor-pointer' 
-              onClick={newEnviroment.length>=1?null:handleNewInv}>
-                {newEnviroment.length>=1?<>Edit</>:<>Add</>}</p>
+      <div
+        className="w-[700px] h-[400px] border bg-gray-50 shadow-xl rounded-md absolute
+       right-[58px] top-11 py-2 flex flex-col justify-between"
+      >
+        {loader === true ? (
+          <EnvLoader />
+        ) : (
+          <>
+            <div className="w-full  flex flex-col">
+              <div className="bg-gray-100 w-full  flex flex-col gap-5  p-2">
+                <div className="flex justify-between">
+                  <p className="text-sm">Enviroment</p>
+                  <p
+                    className="text-sm text-blue-500 cursor-pointer"
+                    onClick={newEnviroment.length >= 1 ? null : handleNewInv}
+                  >
+                    {newEnviroment.length >= 1 ? <>Edit</> : <>Add</>}
+                  </p>
+                </div>
+                {newEnviroment.length >= 1 ? (
+                  <>
+                    <div className="flex">
+                      <p className="w-1/5 text-xs text-gray-700 font-bold">
+                        VARIABLE
+                      </p>
+                      <p className="w-2/5 text-xs text-gray-700 font-bold">
+                        INITIAL VALUE
+                      </p>
+                      <p className="w-2/5 text-xs text-gray-700 font-bold">
+                        CURRENT VALUE
+                      </p>
+                    </div>
+                  </>
+                ) : (
+                  <div className="flex items-center flex-col gap-2">
+                    <p className="w-full text-center text-sm font-medium">
+                      No active Environment
+                    </p>
+                    <p className="text-xs">
+                      An environment is a set of variables that allow you to
+                      switch the context of your requests.
+                    </p>
+                  </div>
+                )}
+              </div>
+              <div className="w-full h-[120px] overflow-y-scroll scrollbar-hide">
+                {newEnviroment.map((e) => (
+                  <div
+                    key={e._id}
+                    className="w-full  flex p-2 hover:bg-gray-200"
+                  >
+                    <p className="w-1/5 text-xs text-gray-700 font-medium">
+                      {e.details[0].variable}
+                    </p>
+                    <p className="w-2/5 text-xs text-gray-700 font-medium">
+                      {e.details[0].value}
+                    </p>
+                    <p className="w-2/5 text-xs text-gray-700 font-medium">
+                      {e.details[0].value}
+                    </p>
+                  </div>
+                ))}
+              </div>
             </div>
-            {newEnviroment.length>=1?<>
-            <div className="flex">
-               <p className='w-1/5 text-xs text-gray-700 font-bold'>VARIABLE</p>
-               <p className='w-2/5 text-xs text-gray-700 font-bold'>INITIAL VALUE</p>
-               <p className='w-2/5 text-xs text-gray-700 font-bold'>CURRENT VALUE</p>
-            </div></>
-            :<div className="flex items-center flex-col gap-2">
-            <p className='w-full text-center text-sm font-medium'>No active Environment</p>
-            <p className='text-xs'>An environment is a set of variables that allow 
-            you to switch the context of your requests.</p>
-         </div>}
+            {/* golbal variable */}
+            <div className="w-full flex flex-col">
+              <div className="bg-gray-100 w-full  flex flex-col gap-5  p-2">
+                <div className="flex justify-between">
+                  <p className="text-sm">Golbal</p>
+                  <p
+                    className="text-sm text-blue-500 cursor-pointer"
+                    onClick={newEnviroment.length >= 1 ? null : handleNewInv}
+                  >
+                    {newEnviroment.length >= 1 ? <>Edit</> : <>Add</>}
+                  </p>
+                </div>
+                {newEnviroment.length >= 1 ? (
+                  <>
+                    <div className="flex">
+                      <p className="w-1/5 text-xs text-gray-700 font-bold">
+                        VARIABLE
+                      </p>
+                      <p className="w-2/5 text-xs text-gray-700 font-bold">
+                        INITIAL VALUE
+                      </p>
+                      <p className="w-2/5 text-xs text-gray-700 font-bold">
+                        CURRENT VALUE
+                      </p>
+                    </div>
+                  </>
+                ) : (
+                  <div className="flex items-center flex-col gap-2">
+                    <p className="w-full text-center text-sm font-medium">
+                      No global variables
+                    </p>
+                    <p className="text-xs">
+                      Global variables are a set of variables that are always
+                      available in a workspace.
+                    </p>
+                  </div>
+                )}
+              </div>
+              <div className="w-full h-[120px] overflow-y-scroll scrollbar-hide">
+                {newEnviroment.map((e) => (
+                  <div
+                    key={e._id}
+                    className="w-full  flex p-2 hover:bg-gray-200"
+                  >
+                    <p className="w-1/5 text-xs text-gray-700 font-medium">
+                      {e.details[0].variable}
+                    </p>
+                    <p className="w-2/5 text-xs text-gray-700 font-medium">
+                      {e.details[0].value}
+                    </p>
+                    <p className="w-2/5 text-xs text-gray-700 font-medium">
+                      {e.details[0].value}
+                    </p>
+                  </div>
+                ))}
+              </div>
             </div>
-            <div className="w-full h-[120px] overflow-y-scroll scrollbar-hide">
-            {newEnviroment.map(e=>(
-              <div key={e._id} className="w-full  flex p-2 hover:bg-gray-200">
-              <p className='w-1/5 text-xs text-gray-700 font-medium'>{e.details[0].variable}</p>
-              <p className='w-2/5 text-xs text-gray-700 font-medium'>{e.details[0].value}</p>
-              <p className='w-2/5 text-xs text-gray-700 font-medium'>{e.details[0].value}</p>
-           </div>
-            ))}
-            </div>
-          </div>
-          {/* golbal variable */}
-          <div className="w-full flex flex-col">
-          <div className="bg-gray-100 w-full  flex flex-col gap-5  p-2">
-            <div className="flex justify-between">
-              <p className='text-sm'>Golbal</p>
-              <p className='text-sm text-blue-500 cursor-pointer'
-              onClick={newEnviroment.length>=1?null:handleNewInv}>
-                {newEnviroment.length>=1?<>Edit</>:<>Add</>}</p>
-            </div>
-            {newEnviroment.length>=1?<>
-            <div className="flex">
-               <p className='w-1/5 text-xs text-gray-700 font-bold'>VARIABLE</p>
-               <p className='w-2/5 text-xs text-gray-700 font-bold'>INITIAL VALUE</p>
-               <p className='w-2/5 text-xs text-gray-700 font-bold'>CURRENT VALUE</p>
-            </div></>
-            :<div className="flex items-center flex-col gap-2">
-            <p className='w-full text-center text-sm font-medium'>No global variables</p>
-            <p className='text-xs'>Global variables are a set of variables that are always available in a workspace.</p>
-         </div>}
-            </div>
-            <div className="w-full h-[120px] overflow-y-scroll scrollbar-hide">
-            {newEnviroment.map(e=>(
-              <div key={e._id} className="w-full  flex p-2 hover:bg-gray-200">
-              <p className='w-1/5 text-xs text-gray-700 font-medium'>{e.details[0].variable}</p>
-              <p className='w-2/5 text-xs text-gray-700 font-medium'>{e.details[0].value}</p>
-              <p className='w-2/5 text-xs text-gray-700 font-medium'>{e.details[0].value}</p>
-           </div>
-            ))}
-            </div>
-            
-          </div>
-        </div>
+          </>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SetEnviroment
+export default SetEnviroment;
