49d5e054ac876484f8c1225b514390331ade8950
ani07ket <aniket.suvaidyam@gmail.com>
2023-02-11 15:59:34 +0530

    left side bar fix

--- src/Components/Home/LeftBar/LeftBar.js
+++ src/Components/Home/LeftBar/LeftBar.js
@@ -7,11 +7,15 @@ const LeftBar = () => {
   return (
     <>
       <div className="w-full flex h-full">
-        <div className=" w-1/5 min-w-[75px] px-1 flex flex-col h-full items-center border-r-2 border-l-2">
+        <div className=" w-1/5 min-w-[75px] px-1 flex flex-col h-full border-r-2 gap-1 ">
           <NavLink
             to="collection"
-            className="w-full  flex flex-col items-center justify-center
+            className="w-full  flex flex-col items-center justify-center rounded  mt-1
+  
         py-3 text-xs gap-1"
+            style={({ isActive }) => ({
+              background: isActive ? " #e8eeff  " : "",
+            })}
           >
             <MdOutlineCollectionsBookmark className="text-xl" />
             Collection
@@ -19,21 +23,30 @@ const LeftBar = () => {
 
           <NavLink
             to="api"
-            className="text-xs flex flex-col items-center justify-center py-3 gap-1"
+            className="text-xs flex flex-col items-center  rounded  justify-center py-3 gap-1"
+            style={({ isActive }) => ({
+              background: isActive ? " #e8eeff  " : "",
+            })}
           >
             <BiCollapse className="text-xl" />
             APIs
           </NavLink>
           <NavLink
             to="environment"
-            className="text-xs flex flex-col items-center justify-center py-3 gap-1"
+            className="text-xs flex flex-col items-center  rounded  justify-center py-3 gap-1"
+            style={({ isActive }) => ({
+              background: isActive ? " #e8eeff  " : "",
+            })}
           >
             <BiCodeBlock className="text-xl" />
             Environment
           </NavLink>
           <NavLink
             to="history"
-            className="text-xs flex flex-col items-center justify-center py-3 gap-1"
+            className="text-xs flex flex-col items-center  rounded  justify-center py-3 gap-1"
+            style={({ isActive }) => ({
+              background: isActive ? " #e8eeff  " : "",
+            })}
           >
             <MdHistory className="text-xl" />
             History
