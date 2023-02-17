7232cc7a83e27fe398d6356649e7d19481ac7624
ani07ket <aniket.suvaidyam@gmail.com>
2023-02-17 11:24:03 +0530

    navbar pupup outside click to close

--- src/Components/Header/Navbar/Navbar.js
+++ src/Components/Header/Navbar/Navbar.js
@@ -1,21 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { NavLink } from "react-router-dom";
 import { AiFillCaretDown } from "react-icons/ai";
 import WorkSpaceDropDwon from "../WorkSpaceDropDwon/WorkSpaceDropDown";
 
 const Navbar = () => {
-  const [open, setopen] = useState(false)
+  const [open, setopen] = useState(false);
+  let menuRef = useRef();
+  useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setopen(false);
+      }
+    };
+    document.addEventListener("mousedown", handler);
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    };
+  });
   return (
     <>
-      <div className="flex gap-8 relative">
-        <NavLink to="/workSpace" className="flex items-center gap-2" onClick={()=>setopen(!open)}>
+      <div className="flex gap-8 relative " ref={menuRef}>
+        <NavLink
+          to="/workSpace"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setopen(!open);
+          }}
+        >
           Workspace <AiFillCaretDown className="text-xs" />
         </NavLink>
         <NavLink to="/reports">Reports</NavLink>
         <NavLink to="/explore">Explore</NavLink>
         {/* workSpace */}
-        {(open===true)&&<WorkSpaceDropDwon {...{setopen}}/>}
-        
+        {open && <WorkSpaceDropDwon {...{ setopen }} />}
       </div>
     </>
   );
