5eb533d33d6d4b88f5e23cf6bda65518c742424e
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2023-04-04 11:49:42 +0530

    send response status 404 page render

--- src/Components/Home/Tabs/TabsBody/ErrorScreen.js
+++ src/Components/Home/Tabs/TabsBody/ErrorScreen.js
@@ -4,7 +4,7 @@ import error from "../../../../Assets/error.png";
 const ErrorScreen = () => {
   return (
     <div className="h-full bg-white border">
-      <p className=" font-medium text-lg h-[8%] px-2 text-gray-600">Response</p>
+      <p className=" font-medium h-[8%] px-2 text-gray-600">Response</p>
       <div className="h-[92%] flex flex-col items-center  justify-center">
         <img className="w-36" src={error} alt="" />
       <p className=" text-sm font-mono text-gray-600">
