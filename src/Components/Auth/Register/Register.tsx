056c340b9e213a26ada809f1e9d622ea36256e3d
Ajamat <ajamat.suvaidyam@gmal.com>
2023-07-31 12:15:57 +0530

    add files

--- src/Components/Auth/Register/Register.tsx
+++ src/Components/Auth/Register/Register.tsx
@@ -1,10 +1,20 @@
 import type { FC } from 'react';
 
-interface RegisterProps {}
+interface RegisterProps { }
 
 const Register: FC<RegisterProps> = () => {
     return (
         <>
+            <div className='w-full h-screen'>
+                <div className='w-full h-full flex'>
+                    <div className='w-[60%] bg-red-500'>
+
+                    </div>
+                    <div className='w-[40%] bg-blue-600'>
+
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
