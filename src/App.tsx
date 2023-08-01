7332bcd19ef0303ace8fcfd65d817db623012d0c
Ajamat <ajamat.suvaidyam@gmal.com>
2023-08-01 14:58:17 +0530

    add file context

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Router from './Components/Router/Router';
+import Context from './Context/Context';
 // import Login from './Components/Auth/Login/Login';
 // import Register from './Components/Auth/Register/Register';
 // import Register from './Components/Auth/Register/Register';
@@ -10,10 +11,15 @@ function App() {
   return (
    <>
   
-   {/* <Register/> */}
-   {/* <Login/> */}
-   {/* <ForgetPassword/> */}
-   <Router/>
+   
+<Context>
+
+ <Router/>
+
+</Context>
+
+
+  
    </>
   );
 }
