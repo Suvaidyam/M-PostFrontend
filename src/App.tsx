b434095781b515c2e4511211064ca58e3a9392ee
abhirock74 <abhishek.suvaidyam@gmail.com>
2023-08-08 11:22:29 +0530

    remove extra workspace folder from context folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import './App.css';
 import Router from './Components/Router/Router';
 import Context from './Context/Context';
-// import Login from './Components/Auth/Login/Login';
-// import Register from './Components/Auth/Register/Register';
-// import Register from './Components/Auth/Register/Register';
-// import ForgetPassword from './Components/Auth/ForgetPassword/ForgetPassword'
+
 
 function App() {
   return (
