e3d9d4c50a3ff846992fdb4f3ce1a71f6fe09010
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2023-10-13 14:02:41 +0530

    Add

--- src/Components/Home/LeftBody/CollectionBody/CollectionBody.tsx
+++ src/Components/Home/LeftBody/CollectionBody/CollectionBody.tsx
@@ -1,11 +1,39 @@
-import type { FC } from 'react';
+import { useContext, type FC } from 'react';
+import { AiFillFolder } from 'react-icons/ai';
+import { FiSearch } from 'react-icons/fi';
+import { HiOutlinePlus } from 'react-icons/hi';
+import { MyContext } from '../../../../Context/Context';
 
 interface CollectionBodyProps { }
 
 const CollectionBody: FC<CollectionBodyProps> = () => {
+    const { slide } = useContext(MyContext);
     return (
         <>
-            CollectionBody
+            <div className={`w-full h-full ${slide === true ? 'hidden' : 'block'}`}>
+                <details>
+                    <summary className='border py-2 cursor-pointer flex h-12 pl-2'>
+                        <div>
+                            <div className='flex items-center gap-3'><AiFillFolder className='text-xl' /> New Collection</div>
+                            <div><p className='text-xs text-blue-600 pl-8 -mt-1'>36 item</p></div>
+                        </div>
+                    </summary>
+                    <div className=''>
+                        <p className='border p-2'>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions, award-winning fireworks and seasonal special events.</p>
+                    </div>
+                </details>
+                <details>
+                    <summary className='border py-2 cursor-pointer flex h-12 pl-2'>
+                        <div>
+                            <div className='flex items-center gap-3'><AiFillFolder className='text-xl' /> New Collection</div>
+                            <div><p className='text-xs text-blue-600 pl-8 -mt-1'>36 item</p></div>
+                        </div>
+                    </summary>
+                    <div className=''>
+                        <p className='border p-2'>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions, award-winning fireworks and seasonal special events.</p>
+                    </div>
+                </details>
+            </div>
         </>
     );
 }
--- src/Components/Home/LeftBody/LeftBody.tsx
+++ src/Components/Home/LeftBody/LeftBody.tsx
@@ -1,46 +1,22 @@
 import { type FC, useContext } from 'react';
-import { AiFillFolder } from 'react-icons/ai';
-import { FiSearch } from 'react-icons/fi';
-import { HiOutlinePlus } from 'react-icons/hi';
 import { MyContext } from '../../../Context/Context';
+import CollectionBody from './CollectionBody/CollectionBody';
+import ApiBody from './ApiBody/ApiBody';
+import EnvironmentBody from './EnvironmentBody/EnvironmentBody';
+import HistoryBody from './HistoryBody/HistoryBody';
+import SearchBar from '../SearchBar/SearchBar';
 
 interface LeftBodyProps { }
 
 const LeftBody: FC<LeftBodyProps> = () => {
-    const { slide } = useContext(MyContext);
+    const { currentNav } = useContext(MyContext);
     return (
         <>
-            <div className={`w-full h-full ${slide === true ? 'hidden' : 'block'}`}>
-                {/* Search Bar */}
-                <div className='relative flex items-center gap-1'>
-                    <input className='w-[87%] border-2 m-2 py-2 pl-2 pr-5 rounded-sm outline-none bg-gray-100 text-xs text-gray-600' type="text" placeholder='Search..' />
-                    <FiSearch className='absolute right-14' />
-                    <HiOutlinePlus className='text-xl cursor-pointer' />
-                </div>
-                {/* Folder Dropdown */}
-                <details>
-                    <summary className='border py-2 cursor-pointer flex h-12 pl-2'>
-                        <div>
-                            <div className='flex items-center gap-3'><AiFillFolder className='text-xl' /> New Collection</div>
-                            <div><p className='text-xs text-blue-600 pl-8 -mt-1'>36 item</p></div>
-                        </div>
-                    </summary>
-                    <div className=''>
-                        <p className='border p-2'>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions, award-winning fireworks and seasonal special events.</p>
-                    </div>
-                </details>
-                <details>
-                    <summary className='border py-2 cursor-pointer flex h-12 pl-2'>
-                        <div>
-                            <div className='flex items-center gap-3'><AiFillFolder className='text-xl' /> New Collection</div>
-                            <div><p className='text-xs text-blue-600 pl-8 -mt-1'>36 item</p></div>
-                        </div>
-                    </summary>
-                    <div className=''>
-                        <p className='border p-2'>Epcot is a theme park at Walt Disney World Resort featuring exciting attractions, international pavilions, award-winning fireworks and seasonal special events.</p>
-                    </div>
-                </details>
-            </div>
+            <SearchBar/>
+            {currentNav === "Collection" && <CollectionBody />}
+            {currentNav === "APIs" && <ApiBody />}
+            {currentNav === "Enviroment" && <EnvironmentBody />}
+            {currentNav === "History" && <HistoryBody />}
         </>
     );
 }
