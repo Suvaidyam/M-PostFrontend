42ca2a302dc7c31b1ca6ac9770b56f7db6d73bc5
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2023-10-17 10:57:26 +0530

    Add

---
+++ src/Components/Loader/Loader.tsx
@@ -0,0 +1,44 @@
+export const EnvLoader = () => {
+    return (
+      <>
+        <div className="h-full w-full">
+          <div className=" flex flex-col gap-1">
+            <div className="h-16 w-full animate-pulse leading-relaxed  flex flex-col justify-between bg-gray-300 p-2">
+              <div className="w-32 h-5 animate-pulse leading-relaxed rounded-sm bg-gray-400"></div>
+              <div className="w-full flex gap-2">
+                <div className="h-5 w-1/5 animate-pulse leading-relaxed rounded-sm bg-gray-400"></div>
+                <div className="h-5 w-2/5 animate-pulse leading-relaxed rounded-sm bg-gray-400"></div>
+                <div className="h-5 w-3/5 animate-pulse leading-relaxed rounded-sm bg-gray-400"></div>
+              </div>
+            </div>
+            <div className="h-8  w-full animate-pulse leading-relaxed  bg-gray-200"></div>
+            <div className="h-8  w-full animate-pulse leading-relaxed  bg-gray-200"></div>
+            <div className="h-8  w-full animate-pulse leading-relaxed  bg-gray-200"></div>
+            <div className="h-8  w-full animate-pulse leading-relaxed  bg-gray-200"></div>
+            <div className="h-16 w-full animate-pulse leading-relaxed  flex flex-col justify-between bg-gray-300 p-2">
+              <div className="w-32 h-5 animate-pulse leading-relaxed rounded-sm bg-gray-400"></div>
+              <div className="w-full flex gap-2">
+                <div className="h-5 w-1/5 animate-pulse leading-relaxed rounded-sm bg-gray-400"></div>
+                <div className="h-5 w-2/5 animate-pulse leading-relaxed rounded-sm bg-gray-400"></div>
+                <div className="h-5 w-3/5 animate-pulse leading-relaxed rounded-sm bg-gray-400"></div>
+              </div>
+            </div>
+            <div className="h-8  w-full animate-pulse leading-relaxed  bg-gray-200"></div>
+            <div className="h-8  w-full animate-pulse leading-relaxed  bg-gray-200"></div>
+            <div className="h-8  w-full animate-pulse leading-relaxed  bg-gray-200"></div>
+          </div>
+        </div>
+      </>
+    );
+  };
+  
+  export const CollectionLoader = () => {
+    return (
+      <>
+        <div className=" flex flex-col items-center   mx-2 ">
+          <div className="bg-gray-300 animate-pulse leading-relaxed h-8 w-full mt-1"></div>
+        </div>
+      </>
+    );
+  };
+  
