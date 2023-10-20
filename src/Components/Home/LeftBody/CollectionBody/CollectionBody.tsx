08261c3d32a8bfdae463abbd1b3a10dee276c4e4
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2023-10-20 16:10:06 +0530

    Add

--- src/Components/Home/LeftBody/CollectionBody/CollectionBody.tsx
+++ src/Components/Home/LeftBody/CollectionBody/CollectionBody.tsx
@@ -9,10 +9,23 @@ import http from '../../../../Service/http';
 import { toast } from 'react-toastify';
 
 interface CollectionBodyProps { }
+interface Details {
+    method?: string | undefined;
+}
+
+interface Colors {
+    [key: string]: string;
+    GET: string;
+    POST: string;
+    PUT: string;
+    DELETE: string;
+    NA: string;
+}
 
 const CollectionBody: FC<CollectionBodyProps> = () => {
-    const { collection, setActiveOption, setCollection, loader, setLoader } = useContext(MyContext);
+    const { collection, setActiveOption, setCollection, loader, setLoader, tabsList, setTabsList, setCurrentActive, setTabData, currentActive } = useContext(MyContext);
     const newArray = collection?.filter((e: any) => e.parent == null);
+    const [array, setArray] = useState(newArray)
     const [toggleFolder, setToggleFolder] = useState<boolean>(false);
     const [activeFolder, setActiveFolder] = useState<string>('');
 
@@ -23,6 +36,21 @@ const CollectionBody: FC<CollectionBodyProps> = () => {
     const ClickOption = (item: string) => {
         setActiveOption(item);
     };
+
+    // Color 
+
+    const getDetails = (details: Details) => {
+        const method: string = details?.method ? details.method.toUpperCase() : "NA";
+        const colors: Colors = {
+            GET: "green",
+            POST: "blue",
+            PUT: "yellow",
+            DELETE: "red",
+            NA: "grey",
+        };
+
+        return { method, color: colors[method.toUpperCase()] };
+    }
 
     // Find Collection Data
     const getData = () => {
@@ -64,6 +92,20 @@ const CollectionBody: FC<CollectionBodyProps> = () => {
             });
     };
 
+    // Handle Request
+
+    const handleRequest = (e: { _id: any; }) => {
+        if (tabsList.findIndex((f: { _id: any; }) => f._id === e._id) < 0) {
+            setTabsList([...tabsList, e]);
+            setCurrentActive(e._id);
+            setTabData(e);
+        }
+    };
+    const openRequest = (ce: any) => {
+        ce.openRequest = !ce.openRequest;
+        setArray([...array]);
+    };
+
     useEffect(() => {
         getData();
     }, [loader]);
@@ -71,45 +113,50 @@ const CollectionBody: FC<CollectionBodyProps> = () => {
     return (
         <>
             <BodyHead {...{ postData, title: "Create collection" }} />
-            {newArray?.map((item: any) => (
-                <div key={item._id} >
+            {newArray?.map((e: any) => (
+                <div key={e._id} >
                     <div className='flex gap-2 relative group justify-between border-b h-9 items-center cursor-pointer'>
                         <div className='w-[80%] justify-start flex truncate'>
-                            <div onClick={() => ClickFolder(item._id)} className='w-10 h-full flex items-center justify-center text-lg'>
-                                {(toggleFolder === true && item._id === activeFolder) ? <BiCaretDown /> : < BiCaretRight />}
+                            <div onClick={() => ClickFolder(e._id)} className='w-10 h-full flex items-center justify-center text-lg'>
+                                {(toggleFolder === true && e._id === activeFolder) ? <BiCaretDown /> : < BiCaretRight />}
                             </div>
                             <div className='text-xl pr-2'>
                                 <FcFolder />
                             </div>
                             <div className='text-sm'>
-                                {item.name}
+                                {e.name}
                             </div>
                         </div>
-                        <div onClick={() => ClickOption(item)} className="hidden group-hover:block absolute right-2">
+                        <div onClick={() => ClickOption(e)} className="hidden group-hover:block absolute right-2">
                             <MoreAction />
                         </div>
                     </div>
-                    {collection?.map((e: any) => (
-                        <div key={e._id}>
-                            {(toggleFolder === true && item._id === activeFolder) ?
-                                <div>
-                                    {item._id === e.parent ?
-                                        (
-                                            <div className="w-full flex justify-between border-b items-center">
-                                                <div className='w-full items-center pl-[15%] flex gap-3 h-7  text-xs'>
-                                                    <p>{(e?.details).method}</p>
-                                                    <p>{e?.name}</p>
-                                                </div>
-                                                <div>
+                    {(toggleFolder === true && e._id === activeFolder) ?
+                        <div className=" w-full">
+                            {collection?.map((ce: any) => (
+                                <div key={ce._id}>
+                                    {e._id === ce.parent ? (
+                                        <div className={`w-full relative group flex cursor-pointer py-1 px-2 
+                                                        ${currentActive === ce._id ? 'bg-gray-300' : ' hover:bg-gray-200'}`} >
+                                            <div className="flex items-center gap-2 w-full " onClick={() => handleRequest(ce)}>
+                                                <p className={`text-[11px] font-semibold text-${getDetails(ce?.details).color
+                                                    }-600 w-[70px] min-w-[70px] flex justify-end`} >
+                                                    {getDetails(ce?.details).method}
+                                                </p>
+                                                <p className="text-xs text-gray-600 font-normal truncate">
+                                                    {ce.name}
+                                                </p>
+                                            </div>
+                                            <p className="hidden group-hover:block absolute right-2">
+                                                <div onClick={() => openRequest(ce)} className='flex justify-center items-center'>
                                                     <RequestAction />
                                                 </div>
-                                            </div>
-                                        ) : null}
-                                </div>
-                                : null}
-                        </div>
-                    ))}
+                                            </p>
 
+                                        </div>) : null}
+                                </div>))}
+                        </div>
+                        : null}
                 </div>
             ))}
         </>
