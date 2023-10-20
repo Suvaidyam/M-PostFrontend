ba790487fa8ba2302bffc7f02cd0ebaa148e0ac1
Abhishek Yadav <abhi.suvaidyam@gmail.com>
2023-10-20 17:10:11 +0530

    Add

--- src/Components/Common/CreateWorkSpace/CreateWorkSpace.tsx
+++ src/Components/Common/CreateWorkSpace/CreateWorkSpace.tsx
@@ -1,7 +1,10 @@
-import type { FC, SetStateAction } from 'react';
+import { FC, SetStateAction ,useContext} from 'react';
 import { Fragment, useRef, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { AiOutlineClose } from 'react-icons/ai';
+import http from '../../../Service/http';
+import { MyContext } from '../../../Context/Context';
+import { toast } from 'react-toastify';
 
 interface CreateWorkSpaceProps {
     open: boolean
@@ -9,6 +12,29 @@ interface CreateWorkSpaceProps {
 };
 
 const CreateWorkSpace: FC<CreateWorkSpaceProps> = ({ open, setOpen }) => {
+    const [name, setName] = useState("");
+    const [visibility, setVisibility] = useState("");
+    const {loader, setLoader} = useContext(MyContext);
+
+    const postData = () => {
+        http({
+            method: "post",
+            url: `${process.env.REACT_APP_BASEURL}/workspace`,
+            data: {
+                name: name,
+                visibility: visibility,
+            },
+        })
+            .then((res) => {
+                setOpen(false);
+                console.log(res);
+                setLoader(!loader);
+                toast.success(res.data.message);
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+    };
     const cancelButtonRef = useRef(null);
     return (
         <>
@@ -54,7 +80,7 @@ const CreateWorkSpace: FC<CreateWorkSpaceProps> = ({ open, setOpen }) => {
                                                     type="text"
                                                     id="name"
                                                     className="outline-blue-500 border-[1.5px] px-2 w-full rounded-sm py-1"
-                                                // onChange={(e) => setName(e.target.value)}
+                                                    onChange={(e) => setName(e.target.value)}
                                                 />
                                             </div>
                                             <div className="mt-4 space-y-4">
@@ -67,7 +93,7 @@ const CreateWorkSpace: FC<CreateWorkSpaceProps> = ({ open, setOpen }) => {
                                                             name="candidates"
                                                             type="radio"
                                                             className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                                        // onChange={(e) => setVisibility(e.target.value)}
+                                                            onChange={(e) => setVisibility(e.target.value)}
                                                         />
                                                     </div>
                                                     <div className="ml-3 text-sm">
@@ -85,7 +111,7 @@ const CreateWorkSpace: FC<CreateWorkSpaceProps> = ({ open, setOpen }) => {
                                                             name="candidates"
                                                             type="radio"
                                                             className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                                        // onChange={(e) => setVisibility(e.target.value)}
+                                                            onChange={(e) => setVisibility(e.target.value)}
                                                         />
                                                     </div>
                                                     <div className="ml-3 text-sm">
@@ -104,7 +130,7 @@ const CreateWorkSpace: FC<CreateWorkSpaceProps> = ({ open, setOpen }) => {
                                                             name="candidates"
                                                             type="radio"
                                                             className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                                        // onChange={(e) => setVisibility(e.target.value)}
+                                                            onChange={(e) => setVisibility(e.target.value)}
                                                         />
                                                     </div>
                                                     <div className="ml-3 text-sm">
@@ -118,120 +144,18 @@ const CreateWorkSpace: FC<CreateWorkSpaceProps> = ({ open, setOpen }) => {
                                             <button
                                                 className="px-4 py-2 bg-blue-500 rounded-md text-xs font-medium text-white mt-3
            "
-                                            // onClick={postData}
+                                            onClick={postData}
                                             >
                                                 Create Workspace
                                             </button>
                                         </div>
                                     </div>
-                                    {/* <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6 rounded-t-md">
-                                        <button
-                                            type="button"
-                                            className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                                            onClick={() => setOpen(false)}
-                                        >
-                                            Deactivate
-                                        </button>
-                                        <button
-                                            type="button"
-                                            className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                                            onClick={() => setOpen(false)}
-                                            ref={cancelButtonRef}
-                                        >
-                                            Cancel
-                                        </button>
-                                    </div> */}
                                 </Dialog.Panel>
                             </Transition.Child>
                         </div>
                     </div>
                 </Dialog>
             </Transition.Root>
-
-
-
-
-            {/* <div className="w-full min-w-[500px]">
-                <p className="text-gray-700 text-xl font-medium">Create workspace</p>
-                <div className="w-full mt-2">
-                    <div>
-                        <label htmlFor="name" className="text-sm font-medium text-gray-500">
-                            Name
-                        </label>
-                        <input
-                            type="text"
-                            id="name"
-                            className="outline-blue-500 border-[1.5px] px-2 w-full rounded-sm py-1"
-                        // onChange={(e) => setName(e.target.value)}
-                        />
-                    </div>
-                    <div className="mt-4 space-y-4">
-                        <p className="text-sm text-gray-600 font-semibold">Visibility</p>
-                        <div className="flex items-start">
-                            <div className="flex h-5 items-center">
-                                <input
-                                    value="PERSONAL"
-                                    id="personal"
-                                    name="candidates"
-                                    type="radio"
-                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                // onChange={(e) => setVisibility(e.target.value)}
-                                />
-                            </div>
-                            <div className="ml-3 text-sm">
-                                <label htmlFor="personal" className="cursor-pointer text-gray-700">
-                                    Personal
-                                </label>
-                                <p className="text-gray-500 text-xs">Only you can access</p>
-                            </div>
-                        </div>
-                        <div className="flex items-start">
-                            <div className="flex h-5 items-center">
-                                <input
-                                    value="TEAM"
-                                    id="comments"
-                                    name="candidates"
-                                    type="radio"
-                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                // onChange={(e) => setVisibility(e.target.value)}
-                                />
-                            </div>
-                            <div className="ml-3 text-sm">
-                                <label htmlFor="comments" className=" cursor-pointer text-gray-700">
-                                    Team
-                                </label>
-                                <p className="text-gray-500 text-xs">All team members can access</p>
-                            </div>
-                        </div>
-
-                        <div className="flex items-start">
-                            <div className="flex h-5 items-center">
-                                <input
-                                    value="PUBLIC"
-                                    id="offers"
-                                    name="candidates"
-                                    type="radio"
-                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                // onChange={(e) => setVisibility(e.target.value)}
-                                />
-                            </div>
-                            <div className="ml-3 text-sm">
-                                <label htmlFor="offers" className=" cursor-pointer text-gray-700">
-                                    Public
-                                </label>
-                                <p className="text-gray-500 text-xs">Everyone can view</p>
-                            </div>
-                        </div>
-                    </div>
-                    <button
-                        className="px-4 py-2 bg-blue-500 rounded-md text-xs font-medium text-white mt-3
-           "
-                    // onClick={postData}
-                    >
-                        Create Workspace
-                    </button>
-                </div>
-            </div> */}
         </>
     );
 }
