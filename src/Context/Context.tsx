0b3797d6b61823a5f64d5782e15573f0a5c01726
Sujeet Yadav <sujeetyadav.suvaidyam@gmail.com>
2023-11-23 14:43:13 +0530

    add

--- src/Context/Context.tsx
+++ src/Context/Context.tsx
@@ -137,7 +137,7 @@ const Context: FC<ContextProps> = ({ children }) => {
     // Set Workspace
     const [workspace, setWorkspace] = useState<any>([]);
     const [selected, setSelected] = useState<string>("json");
-    const [bodyTab, setBodyTab] = useState<string>("Body");
+    const [bodyTab, setBodyTab] = useState<string>("Params");
     const [trash, setTrash] = useState<boolean>(false);
     const [codeMethod, setCodeMethod] = useState<boolean>(false);
 
